372eb18aa45b1ba60c9a949d392d35741db2446f
Hilman Ibnu <hia.develop@gmail.com>
2019-12-18 16:45:20 +0700

    hapus, try total

--- components/Troli.js
+++ components/Troli.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import { View, Text, BUtton, StyleSheet, SafeAreaView, FlatList, TouchableOpacity} from 'react-native';
+import { View, Text, BUtton, StyleSheet, SafeAreaView, FlatList, TouchableHighlight, Alert, Button} from 'react-native';
 import ActionButton from 'react-native-action-button';
 import Constants from 'expo-constants';
 import * as SQLite from 'expo-sqlite';
@@ -8,7 +8,8 @@ const db = SQLite.openDatabase('bookshop.db');
 
 export default class Troli extends Component {
     state = {
-        troli : []
+        troli : [],
+        total:0
     }
 
     static navigationOptions = {
@@ -29,29 +30,112 @@ export default class Troli extends Component {
             
             });
         });
+        this.forceUpdateHandler = this.forceUpdateHandler.bind(this);
+    }
+
+    forceUpdateHandler(){
+        this.forceUpdate();
+    };
+
+    hapusTroli = (item, index) => {
+        Alert.alert(
+            'Konfirmasi',
+            'Menghapus item?',
+            [
+                {
+                    text: 'Batal',
+                    onPress: () => console.log('Cancel Pressed'),
+                    style: 'cancel', 
+                },
+                {
+                    text: 'Hapus',
+                    onPress: () => {
+                        db.transaction(
+                            tx => {
+                                tx.executeSql("DELETE FROM troli WHERE id=?",[item.id])
+                            },
+                            error => {
+                                alert(error);
+                            },
+                            () => {
+                                var dAr = this.state.troli;
+                                var pos = dAr.indexOf(item);
+                                dAr.splice(pos,1);
+                                this.setState({troli:dAr})                            }
+                        );
+                    }
+                },
+            ],
+            {cancelable: true},
+        );
+    }
+
+    checkout = () => {
+        Alert.alert(
+            'Konfirmasi',
+            'Apakah ingin checkout?',
+            [
+                {
+                    text: 'Batal',
+                    onPress: () => console.log('Cancel Pressed'),
+                    style: 'cancel', 
+                },
+                {
+                    text: 'Checkout',
+                    onPress: () => {
+                        db.transaction(
+                            tx => {
+                                tx.executeSql("DELETE FROM troli")
+                            },
+                            error => {
+                                alert(error);
+                            },
+                            () => {
+                                var dAr = this.state.troli;
+                                dAr.splice(0);
+                                this.setState({troli:dAr})                            
+                            }
+                        );
+                    }
+                },
+            ],
+            {cancelable: true},
+        );
     }
 
     render() {
         const {navigation} = this.props;
-
+        let tmp = 0;
         return (
             <View style={{flex:1, backgroundColor: '#f3f3f3'}}>
                 <SafeAreaView style={styles.container}>
+                    
                     <FlatList
                         data={this.state.troli}
-                        renderItem={({item}) => {
+                        renderItem={({item, index}) => {
                             let subtotal = item.price * item.qty;
+                            tmp = tmp+subtotal;
+                            
                             return (
-                                <View style={{marginLeft: 20}}>
+                                <View style={{marginLeft: 10}}>
                                     <Text style={{fontSize: 20}}>Title = {item.title} </Text>
                                     <Text style={{fontSize: 20}}>Price = {item.price} </Text>
                                     <Text style={{fontSize: 20}}>Qty = {item.qty}</Text>
-                                    <Text style={{fontSize: 20}}>Subtotal = {item.subtotal} </Text>
+                                    <Text style={{fontSize: 20}}>Subtotal = {subtotal} </Text>
+                                    <TouchableHighlight style={{width:100,backgroundColor: '#F24405', justifyContent: 'center', alignItems: 'center', borderRadius: 20}} onPress={() => this.hapusTroli(item,index)}>
+                                        <Text style={styles.fullWidthButtonText}>Hapus</Text>
+                                    </TouchableHighlight> 
                                     <Text>-----------------------------------------------------------------</Text>
                                 </View>
                             );
                         }}
                         keyExtractor={item => item.isbn13}/>
+                        <View style={{marginBottom:10, marginTop:10}}>
+                            <Text style={{fontSize: 20, marginLeft:10, marginBottom:20}}>Total = {tmp} </Text>
+                            <Button style={styles.fullWidthButton}
+                                onPress={()=> this.checkout()}
+                                title="Checkout"/>
+                        </View>
                 </SafeAreaView>
             </View>
         )
@@ -71,5 +155,18 @@ const styles = StyleSheet.create({
     },
     title: {
         fontSize: 30,
-    }
+    },
+    fullWidthButton: {
+        width: 200,
+        backgroundColor: '#F24405',
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius:20
+    },
+    fullWidthButtonText: {
+        margin:10,
+        fontSize:10,
+        color: 'white'
+    },
 });
